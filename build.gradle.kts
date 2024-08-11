e9cf269039a64c78b55b380aca77769dc1e0001f
Shane Witbeck <shane@surly.dev>
2024-08-11 08:57:12 -0700

    build: Add maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     kotlin("jvm") version "2.0.10"
     kotlin("plugin.serialization") version "2.0.10"
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
 group = "dev.surly"
@@ -32,6 +34,38 @@ publishing {
             groupId = "dev.surly"
             artifactId = "kotlin-minimal-server"
             version = "0.1.0"
+        }
+    }
+}
+
+
+mavenPublishing {
+    coordinates("dev.surly", "kotlin-minimal-server", "0.1.0")
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+    pom {
+        name.set("Kotlin Minimal Server")
+        description.set("A near zero dependency Kotlin web server")
+        inceptionYear.set("2024")
+        url.set("https://github.com/thesurlydev/kotlin-minimal-server/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("thesurlydev")
+                name.set("Shane Witbeck")
+                url.set("https://github.com/thesurlydev/")
+            }
+        }
+        scm {
+            url.set("https://github.com/thesurlydev/kotlin-minimal-server/")
+            connection.set("scm:git:git://github.com/thesurlydev/kotlin-minimal-server.git")
+            developerConnection.set("scm:git:ssh://git@github.com/thesurlydev/kotlin-minimal-server.git")
         }
     }
 }
